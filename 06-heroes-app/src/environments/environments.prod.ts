c93957cc66d2fff7725c8aa8d6c85493f6b799ff
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2023-09-21 13:56:50 -0600

    x

--- 06-heroes-app/src/environments/environments.prod.ts
+++ 06-heroes-app/src/environments/environments.prod.ts
@@ -1,6 +1,5 @@
 
 
 export const environments = {
-  // baseUrl: 'https://heroes-app-angular.up.railway.app/api',
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'https://heroes-app-angular.up.railway.app',
 }
